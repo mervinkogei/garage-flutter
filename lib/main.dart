5cb3034019a66e003b0914d3395824dbeaa1ec0d
vincent kipkirui kogei <vincent@moovn.com>
2021-02-23 20:35:43 +0300

    add menu and carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
-  MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
-  );
+  ));
 }
 
 class HomePage extends StatefulWidget {
@@ -15,8 +17,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-          appBar: AppBar(
+    Widget image_carousel = Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/audio.jpg'),
+          AssetImage('images/brake.jpg'),
+          AssetImage('images/engine.jpg'),
+          AssetImage('images/paint.jpg'),
+          AssetImage('images/wheel.jpg'),
+          AssetImage('images/type.png'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(microseconds: 1000),
+        dotSize: 4.0,
+        dotBgColor: Colors.transparent,
+        indicatorBgPadding: 4.0,
+      ),
+    );
+      return Scaffold(
+      // body: Center(child: Text('Welcome')),
+      appBar: AppBar(
         elevation: 0.1,
         backgroundColor: Colors.redAccent,
         title: Text('Garage App'),
@@ -35,7 +58,98 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {}),
         ],
       ),
+      drawer: Drawer(
+        child: ListView(children: [
+            UserAccountsDrawerHeader(
+              accountName: Text('Vincent'),
+              accountEmail: Text('vincent@moovn.com'),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.redAccent),
+            ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Home',style: TextStyle(color: Colors.blueAccent)),
+              leading: Icon(
+                Icons.home, color: Colors.redAccent,
+              ),
+          )
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My Account',style: TextStyle(color: Colors.blueAccent)),
+              leading: Icon(
+                Icons.person, color: Colors.redAccent,
+              ),
+          )
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Category',style: TextStyle(color: Colors.blueAccent),),
+              leading: Icon(
+                Icons.dashboard, color: Colors.redAccent,
+              ),
+          )
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Payment',style: TextStyle(color: Colors.blueAccent)),
+              leading: Icon(
+                Icons.payment, color: Colors.redAccent,
+              ),
+          )
+          ),
+           Divider(),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(
+                Icons.settings
+              ),
+          )
+          ),
+
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('About'),
+              leading: Icon(
+                Icons.help, color: Colors.blueAccent,
+              ),
+          )
+          ),
+
+        ],),
+      ),
+        body: ListView(
+        children: <Widget>[
+          //Image Carousel
+          image_carousel,
+          //Padding
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text('Categories',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+        ],
+        )
       
     );
   }
 }
+
+
